cee9e7154f3f7c9af328a12fdc2bb17b46638ab0
Ilya <ilyalutskiy@gmail.com>
2018-10-13 15:57:03 -0500

    final push

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Philly Cheese Steak Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Veggie Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Deep Fried Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('PBJ Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Steak Burger', true);
